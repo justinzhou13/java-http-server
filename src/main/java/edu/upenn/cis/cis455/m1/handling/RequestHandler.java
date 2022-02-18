e73cf8ed6dfce375a03251d1356155045dd77bd9
Justin Zhou <justinzhou13@gmail.com>
2022-02-17 23:02:01 -0500

    successfully swapped out route map in favor of route tree

--- src/main/java/edu/upenn/cis/cis455/m1/handling/RequestHandler.java
+++ src/main/java/edu/upenn/cis/cis455/m1/handling/RequestHandler.java
@@ -18,16 +18,11 @@ public class RequestHandler {
 
 	static final Logger logger = LogManager.getLogger(RequestHandler.class);
 
-	private static final Map<String, Map<String, Route>> routes;
 	private static final Map<String, PathStep> routeTrees;
 
 	private static final GetFileRoute getFileRoute = new GetFileRoute();
 
 	static {
-		routes = new HashMap<>();
-		routes.put("GET", new HashMap<>());
-		routes.put("HEAD", new HashMap<>());
-
 		routeTrees = new HashMap<>();
 		routeTrees.put("GET", new PathStep(""));
 		routeTrees.put("HEAD", new PathStep(""));
@@ -44,21 +39,21 @@ public class RequestHandler {
 		checkProtocolSupported(req);
 		if (isCompliant(req, res)) {
 			String requestMethod = req.requestMethod().equals("HEAD") ? "GET" : req.requestMethod();
-			boolean lookingForFile = requestMethod.equals("GET")
-					&& !routes.get("GET").containsKey(req.pathInfo());
+			PathStep routeTreeRoot = routeTrees.get(requestMethod);
+			if (routeTreeRoot == null) throw new HaltException(501);
+
+			Map<String, String> pathParams = new HashMap<>();
+			Route requestedRoute = getRoute("GET", req.pathInfo(), pathParams);
+			boolean lookingForFile = requestMethod.equals("GET") && requestedRoute == null;
 			if (lookingForFile) {
 				handleFileRequest(req, res);
 				return;
 			}
 
-			Map<String, Route> routesForMethod = routes.get(requestMethod);
-			if (routesForMethod == null) throw new HaltException(501);
-
-			Route route = routesForMethod.get(req.pathInfo());
-			if (route == null) throw new HaltException(404);
+			if (requestedRoute == null) throw new HaltException(404);
 
 			try {
-				route.handle(req, res);
+				requestedRoute.handle(req, res);
 			} catch (Exception e){
 				logger.error(e.getMessage());
 				throw new HaltException(500);
@@ -70,20 +65,11 @@ public class RequestHandler {
 		getFileRoute.handle(req, res);
 	}
 
-	public static void addRoute(String httpMethod, String uri, Route route) {
-		try {
-			logger.info(String.format("attempting to add new route to worker for method %s and uri %s",
-							httpMethod,
-							uri));
-			synchronized (routes) {
-				routes.get(httpMethod).put(uri, route);
-			}
-		} catch (NullPointerException e) {
-			logger.error("Attempted to add a route that wasn't a valid HTTP method");
-		}
+	public static void addRoute(String httpMethod, String path, Route route) {
+		addRouteToTree(httpMethod, path, route);
 	}
 
-	public static void addRouteToTree(String httpMethod, String path, edu.upenn.cis.cis455.m2.interfaces.Route route) {
+	public static void addRouteToTree(String httpMethod, String path, Route route) {
 		synchronized (routeTrees) {
 			PathStep root;
 			if (!routeTrees.containsKey(httpMethod)) {
@@ -98,7 +84,7 @@ public class RequestHandler {
 		}
 	}
 
-	public static edu.upenn.cis.cis455.m2.interfaces.Route getRoute(String httpMethod, String path, Map<String, String> params) {
+	public static Route getRoute(String httpMethod, String path, Map<String, String> params) {
 		synchronized (routeTrees) {
 			if (!routeTrees.containsKey(httpMethod)) {
 				throw new HaltException(501);
